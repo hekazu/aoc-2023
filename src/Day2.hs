c3096af8fb0dfbda51a634ce030495965cf65cd6
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2023-12-02 11:31:30 +0200

    Tidy up day 2 some more, add comments

--- src/Day2.hs
+++ src/Day2.hs
@@ -17,6 +17,7 @@ sumIds = sum . map (read . grabId)
     grabId :: String -> String
     grabId = init . head . drop 1 . words
 
+-- Check whether a game is possible with the given contraints on draws
 isPossibleGame :: String -> Bool
 isPossibleGame = all possible . prepareGameInfo
   where
@@ -26,19 +27,24 @@ isPossibleGame = all possible . prepareGameInfo
       | "blue" `isPrefixOf` colour && x > 14 = False
       | otherwise = True
 
-pairs :: [String] -> [(Int,String)]
-pairs [] = []
-pairs (sx:colour:rest) = (read sx,colour) : pairs rest
-
 part2 :: IO ()
 part2 = do
   input <- readFile "./input.txt"
   print . sum . map gamePower $ lines input
 
+-- Calculate the "power" of a single game
 gamePower :: String -> Int
 gamePower =  product . M.elems . M.fromListWith max . map toAssocList . prepareGameInfo
   where
+    -- Turn colours into association list keys, stripping trailing commas and
+    -- semicolons
     toAssocList = first (filter isAlpha) . swap
 
+-- Take the information about a game and turn it into a list of pairs
+-- representing draws, ordered like in source code
 prepareGameInfo :: String -> [(Int, String)]
 prepareGameInfo = pairs . drop 2 . words
+  where
+    pairs :: [String] -> [(Int,String)]
+    pairs [] = []
+    pairs (sx:colour:rest) = (read sx,colour) : pairs rest
