a980911c43578bdc8b2c13edc0c53414d202ff6c
Henri Peurasaari <henri.peurasaari@helsinki.fi>
2024-01-10 16:00:17 +0200

    Remove barely started day

--- src/Day18.hs
+++
@@ -1,9 +0,0 @@
-module Day18 (part1) where
-
-part1 :: IO ()
-part1 = do
-  input <- map ((\[dir,l] -> (dir, read l)) . take 2 . words) . lines <$> readFile "./sampleinput.txt"
-  print $ dig input
-
-dig :: [(String,Int)] -> Bool
-dig = undefined
